2d230a2da5cef268fbd1400f33832478be11c59a
Ariel Perez <ariel@Ariels-MacBook-Pro.local>
2020-11-08 17:34:27 -0500

    the Core of

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  private firstObsSubs: Subscription;
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubs = interval(1000).subscribe(count => {
+        console.log(count);
+      });
   }
 
+  // To destroy an observable it's necessary to store the subscription
+  //  thar return the observable in a property of type subscription,
+  //  then used in Angular's method onDestroy, to destroy the subscription
+  //  so that also stop the observable.
+  ngOnDestroy(): void {
+    this.firstObsSubs.unsubscribe();
+  }
 }
